4468636d5edcc8fa54ba381eb25215e79b923993
KwakDongGyu <kwakdg0701@naver.com>
2023-05-21 13:15:10 +0900

    Create led.c

---
+++ Device_Driver/temp/led.c
@@ -0,0 +1,50 @@
+#ifdef _MSC_VER
+#define EXPORT __declspec(dllexport)
+#else
+#define EXPORT
+#endif
+
+#include <vector>
+#include <numeric>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <linux/kdev_t.h>
+
+#define LED_FILE_NAME "/dev/led_driver"
+#define SM_FILE_NAME "/dev/step_motor_driver"
+#define PHOTOREGISTER_FILE_NAME "/dev/photoregister_driver"
+
+extern "C"
+{
+    EXPORT void ledOn()
+    {
+        int fd = open(LED_FILE_NAME, O_RDWR);
+        if (fd < 0)
+        {
+            fprintf(stderr, "Can't open %s\n", LED_FILE_NAME);
+            return;
+        }
+        char data = 1;
+
+        write(fd, &data, sizeof(char));
+    }
+
+    EXPORT void ledOff()
+    {
+        int fd = open(LED_FILE_NAME, O_RDWR);
+        if (fd < 0)
+        {
+            fprintf(stderr, "Can't open %s\n", LED_FILE_NAME);
+            return;
+        }
+
+        char data = 0;
+
+        write(fd, &data, sizeof(char));
+    }
+}
