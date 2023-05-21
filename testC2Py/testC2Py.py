f5a9e76f34e9a1bc4762ac28c1fc26cf1343228d
김민관 <alsrhks2508@naver.com>
2023-05-22 05:24:12 +0900

    누나가 작업한 C파일 받아올 양식

--- testC2Py/testC2Py.py
+++ testC2Py/testC2Py.py
@@ -1,5 +1,5 @@
-import ctypes                           # 파이썬 extension을 사용하기 위한 모듈
-import platform                         # 파이썬 아키텍처를 확인하기 위한 모듈
+import ctypes      # 파이썬 extension을 사용하기 위한 모듈
+import platform    # 파이썬 아키텍처를 확인하기 위한 모듈
 import time
 
 if 'Windows' == platform.system() :     # 윈도우 운영체제에서 c 모듈 로드
@@ -14,34 +14,20 @@ else :
 
 def main():
     print(c_module)
-    # int add(int a, int b)
-    """  
+
     # 함수 주소 포인터
-    add = c_module.add  
-    print("add : ", add)   # <_FuncPtr object at 0x000002196BFE9860>
-    
-    # c에서 제작한 add 함수 파라미터 형식 지정
-    add.argtypes = (ctypes.c_int, ctypes.c_int) 
+    ledOn = c_module.ledOn
+    ledOff = c_module.ledOff
 
-    # c에서 제작한 add 함수 리턴 형식 지정
-    add.restype = ctypes.c_int
-
-    # 실제로 사용
-    res = add(1, 2)
-
-    # 결과 출력
-    print('res :', res)   # 3
-
-    """ 
-
-    ledOn = c_module.ledOn()
-    ledOff = c_module.ledOff()
+    # c에서 제작한 함수 파라미터 형식 지정
     ledOn.argtypes = []
-    ledOn.restype = None
-
     ledOff.argtypes = []
+
+    # c에서 제작한 함수 리턴 형식 지정
+    ledOn.restype = None
     ledOff.restype = None
     
+    # 사용부
     while True:
         ledOn()
         time.sleep(1)
