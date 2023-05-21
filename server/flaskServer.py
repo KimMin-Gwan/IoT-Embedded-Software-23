5810ba92164a276c78e94f5f16ddf6d22136c52c
김민관 <alsrhks2508@naver.com>
2023-05-22 05:23:46 +0900

    플라스크 멤버 추가

--- server/flaskServer.py
+++ server/flaskServer.py
@@ -4,11 +4,13 @@ import server
 
 class Server():
     # 생성자
-    def __init__(self, main_function):
+    def __init__(self, info_master, curtain, led_master):
         self.app = Flask(__name__)
         # 외부 클래스는 포인터로 받아와짐
         # 즉, 내부에서 변경되면 외부도 변경됨
-        self.main = main_function 
+        self.info = info_master 
+        self.curtain = curtain
+        self.led = led_master
         self.register_routes()
     
 
@@ -23,18 +25,20 @@ class Server():
         def set_alarm_serv():
             time = request.form['time']
             flag = request.form['flag']
-            self.main.set_alarm_control(flag, time)
+            self.set_alarm(flag, time)
             return 'Success'
         
         #커튼을 열어라
         @self.app.route("/open")
         def open_curtain():
-            self.main.set_curtain_control(False)
+            self.curtain.move_curtain(False)
+            self.led.led_blue_off()
             
         #커튼을 열어라
         @self.app.route("/close")
         def close_curtain():
-            self.main.set_curtain_control(True)
+            self.curtain.move_curtain(True)
+            self.led.led_blue_off()
 
     def run_server(self, host = server.HOST):
         self.app.run(host)
