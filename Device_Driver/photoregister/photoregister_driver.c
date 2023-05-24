c89f534436e70f81c731a549282ffaa59c4dcb77
김민관 <alsrhks2508@naver.com>
2023-05-25 02:34:40 +0900

    오타수정
    6 ~ 13 : lunux -> linux

--- Device_Driver/photoregister/photoregister_driver.c
+++ Device_Driver/photoregister/photoregister_driver.c
@@ -3,14 +3,14 @@
 #include <linux/kernel.h>
 #include <linux/fs.h>
 #include <asm/uaccess.h>
-#include <lunux/slab.h>
-#include <lunux/gpio.h>
-#include <lunux/io.h>
-#include <lunux/poll.h>
-#include <lunux/interrupt.h>
-#include <lunux/irq.h>
-#include <lunux/sched.h>
-#include <lunux/wait.h>
+#include <linux/slab.h>
+#include <linux/gpio.h>
+#include <linux/io.h>
+#include <linux/poll.h>
+#include <linux/interrupt.h>
+#include <linux/irq.h>
+#include <linux/sched.h>
+#include <linux/wait.h>
 
 #define PHOTOREGISTER_MAJOR 222
 #define PHOTOREGISTER_NAME "PHOTOREGISTER_DRIVER"
