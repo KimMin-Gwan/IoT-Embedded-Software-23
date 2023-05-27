655c32597fcaf0d0911faa0dce374db666db72dd
김민관 <alsrhks2508@naver.com>
2023-05-28 05:35:03 +0900

    노드 메이커

--- Device_Driver/nod_make.py
+++ Device_Driver/nod_make.py
@@ -1,23 +1,12 @@
 import os
 import sys
 
-os.system('cd led')
-os.system('ls')
 os.system('mknod /dev/led_driver c 220 0')
-os.system('insmod led_driver.ko')
+os.system('insmod ./led/led_driver.ko')
 
-os.system('cd ..')
-
-os.system('cd step_motor')
-os.system('ls')
 os.system('mknod /dev/step_motor_driver c 221 0')
-os.system('insmod step_motor_driver.ko')
+os.system('insmod ./step_motor/step_motor_driver.ko')
 
-os.system('cd ..')
-
-os.system('cd photoregister')
-os.system('ls')
 os.system('mknod /dev/photoregister_driver c 222 0')
-os.system('insmod photoregister_driver.ko')
+os.system('insmod ./photoregister/photoregister_driver.ko')
 
-os.system('cd ..')
