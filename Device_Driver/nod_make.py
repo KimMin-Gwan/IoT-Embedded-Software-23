85ba449b8e9a308fb6034c33b045757330802d98
KimMin-gwan <alsrhks2508@naver.com>
2023-05-28 05:25:11 +0900

    pi_file

--- Device_Driver/nod_make.py
+++ Device_Driver/nod_make.py
@@ -2,20 +2,22 @@ import os
 import sys
 
 os.system('cd led')
+os.system('ls')
 os.system('mknod /dev/led_driver c 220 0')
-os.system('insnod led_driver.ko')
+os.system('insmod led_driver.ko')
 
 os.system('cd ..')
 
 os.system('cd step_motor')
+os.system('ls')
 os.system('mknod /dev/step_motor_driver c 221 0')
-os.system('insnod step_motor_driver.ko')
+os.system('insmod step_motor_driver.ko')
 
 os.system('cd ..')
 
 os.system('cd photoregister')
+os.system('ls')
 os.system('mknod /dev/photoregister_driver c 222 0')
-os.system('insnod photoregister_driver.ko')
+os.system('insmod photoregister_driver.ko')
 
 os.system('cd ..')
-os.system('dmesg')
---
+++ test_os.py
@@ -0,0 +1,4 @@
+import os, sys
+
+cmd = 'ls'
+os.system(cmd)
