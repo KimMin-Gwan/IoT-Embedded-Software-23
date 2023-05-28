377850a280e45a361e65872568b3a37c9d499d56
김민관 <alsrhks2508@naver.com>
2023-05-29 04:46:57 +0900

    드라이버 및 실행 함수

---
+++ LCD/display.py
@@ -0,0 +1,24 @@
+import I2C_LCD_driver
+from time import *
+
+def change_string(date, time):
+    year = str(date['year'])
+    month = str(date['month'])
+    day = str(date['day'])
+    hour = str(time['hour'])
+    min = str(time['minute'])
+    sec = str(time['second'])
+
+    string_date = year + '년' + " " + month + "월 " + " " + day + "일"
+    string_time = hour + " : " + month + " : " + day
+    return string_date, string_time
+
+
+def display_lcd(info, mylcd):
+    raw_date = info.get_date()
+    raw_time = info.get_time()
+
+    date, time = change_string(raw_date, raw_time)
+
+    mylcd.lcd_display_string(date,1)
+    mylcd.lcd_display_string(time,2)
