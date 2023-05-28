27421d9fb38550c5449b8445e3ddc64b640df6e5
김민관 <alsrhks2508@naver.com>
2023-05-28 19:42:02 +0900

    Update InfoClass.py

--- info/InfoClass.py
+++ info/InfoClass.py
@@ -109,7 +109,8 @@ class Information:
 
         # 초를 시, 분, 초로 변환
         difference_minute = (difference_seconds % 3600) // 60
-        print('diff : ', difference_minute)
+        print('diff : ', difference_seconds)
+        print('diff_m  : ', difference_minute)
 
         return difference_minute
 
