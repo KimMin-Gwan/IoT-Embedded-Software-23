dd39144e936b09a5a3b7368dadc202b20393c7cb
김민관 <alsrhks2508@naver.com>
2023-05-29 04:52:44 +0900

    출력부 수정 및 리셋 추가

--- LCD/display.py
+++ LCD/display.py
@@ -5,12 +5,13 @@ def change_string(date, time):
     year = str(date['year'])
     month = str(date['month'])
     day = str(date['day'])
+    week = date['weekday']
     hour = str(time['hour'])
     min = str(time['minute'])
     sec = str(time['second'])
 
-    string_date = year + '년' + " " + month + "월 " + " " + day + "일"
-    string_time = hour + " : " + month + " : " + day
+    string_date = year + "/" + month + "/" + day +"/"+ week
+    string_time = hour + ":" + month + ":" + day
     return string_date, string_time
 
 
@@ -22,3 +23,7 @@ def display_lcd(info, mylcd):
 
     mylcd.lcd_display_string(date,1)
     mylcd.lcd_display_string(time,2)
+
+def display_reset(mylcd):
+    mylcd.lcd_display_string(" ",1)
+    mylcd.lcd_display_string(" ",2)
