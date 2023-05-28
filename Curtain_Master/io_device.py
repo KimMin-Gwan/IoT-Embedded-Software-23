c1b0a189330a705ff7fea2b32cf54a98a5cb93e8
김민관 <alsrhks2508@naver.com>
2023-05-28 16:47:04 +0900

    동작부를 지우고 프린트문으로 대체

--- Curtain_Master/io_device.py
+++ Curtain_Master/io_device.py
@@ -19,12 +19,15 @@ class Motor:
         # c에서 제작한 함수 리턴 형식 지정
         self.push.argtypes = []
         self.push.argtypes = None
+        print('Motor init complete')
 
     def pull_motor(self):
-        self.pull
+        print('pull')
+        #self.pull
 
     def push_motor(self):
-        self.push
+        print('push')
+        #self.push
     
 
 class Photoresistor:
@@ -38,12 +41,15 @@ class Photoresistor:
         self.resistance_value = self.c_module.get_resistance
         self.resistance_value.argtypes = []
         self.resistance_value.restype = None
+        print('Photoresistor init complete')
 
 
     def get_brigthtness_data(self):
-        result = self.resistance_value()
+        print('get_birthgtness')
+        #result = self.resistance_value()
         # 전처리 필요하면 전처리
-        return result
+        #return result
+        return 0
 
 class Led:
     def __init__(self):
@@ -60,63 +66,12 @@ class Led:
 
         self.ledOff.argtypes = []
         self.ledOff.restype = None
+        print('led init complete')
 
     def led_on(self):
-        self.ledOn()
+        print('ledON')
+        #self.ledOn()
 
     def led_off(self):
-        self.ledOff()
-
-        """
-        # 초기설정
-        self.red_on= self.c_module.red_led_on
-        self.green_on= self.c_module.green_led_on
-        self.blue_on= self.c_module.blue_led_on
-
-        self.red_off= self.c_module.red_led_off
-        self.green_off= self.c_module.green_led_off
-        self.blue_off= self.c_module.blue_led_off
-
-        self.red_on.argtypes = []
-        self.red_on.restype = None
-
-        self.red_off.argtypes = []
-        self.red_off.restype = None
-
-        self.green_on.argtypes = []
-        self.green_on.restype = None
-
-        self.green_off.argtypes = []
-        self.green_off.restype = None
-
-        self.blue_on.argtypes = []
-        self.blue_on.restype = None
-
-        self.blue_off.argtypes = []
-        self.blue_off.restype = None
-
-    def led_red_on(self):
-        self.red_on()
-        return
-
-    def led_red_off(self):
-        self.red_off()
-        return
-
-    def led_green_on(self):
-        self.green_on()
-        return
-
-    def led_green_off(self):
-        self.green_off()
-        return
-
-    def led_blue_on(self):
-        self.blue_on()
-        return
-
-    def led_blue_off(self):
-        self.blue_off()
-        return
-
-    """
+        print('led_off')
+        #self.ledOff()
